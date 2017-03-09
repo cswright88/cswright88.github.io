990d1feaa551344781a222ed4abe091a33a8b5af
cswright88 <cswright88@gmail.com>
2017-03-08 19:19:24 -0600

    add resume

---
+++ resume/js/helper.js
@@ -0,0 +1,120 @@
+
+//BOOTSTRAP VARS
+var bootstrapContainerStart = '<div class="container">%data%</div>';
+var bootstrapRowStart = '<div class="row">%data%</div>';
+//add xs, sm, md, lg 1,4,8,6,12
+//xs
+var bootstrapExtraSmall1Start = '<div class="col-xs-1">%data%</div>';
+var bootstrapExtraSmall3Start = '<div class="col-xs-3">%data%</div>';
+var bootstrapExtraSmall4Start = '<div class="col-xs-4">%data%</div>';
+var bootstrapExtraSmall6Start = '<div class="col-xs-6">%data%</div>';
+var bootstrapExtraSmall8Start = '<div class="col-xs-8">%data%</div>';
+var bootstrapExtraSmall12Start = '<div class="col-xs-12">%data%</div>';
+//sm
+var bootstrapSmall1Start = '<div class="col-sm-1">%data%</div>';
+var bootstrapSmall3Start = '<div class="col-sm-3">%data%</div>';
+var bootstrapSmall4Start = '<div class="col-sm-4">%data%</div>';
+var bootstrapSmall6Start = '<div class="col-sm-6">%data%</div>';
+var bootstrapSmall8Start = '<div class="col-sm-8">%data%</div>';
+var bootstrapSmall12Start = '<div class="col-sm-12">%data%</div>';
+//md
+var bootstrapMedium1Start = '<div class="col-md-1">%data%</div>';
+var bootstrapMedium3Start = '<div class="col-md-3">%data%</div>';
+var bootstrapMedium4Start = '<div class="col-md-4">%data%</div>';
+var bootstrapMedium6Start = '<div class="col-md-6">%data%</div>';
+var bootstrapMedium8Start = '<div class="col-md-8">%data%</div>';
+var bootstrapMedium12Start = '<div class="col-md-12">%data%</div>';
+//lg
+var bootstrapLarge1Start = '<div class="col-lg-1">%data%</div>';
+var bootstrapLarge3Start = '<div class="col-lg-3">%data%</div>';
+var bootstrapLarge4Start = '<div class="col-lg-4">%data%</div>';
+var bootstrapLarge6Start = '<div class="col-lg-6">%data%</div>';
+var bootstrapLarge8Start = '<div class="col-lg-8">%data%</div>';
+var bootstrapLarge12Start = '<div class="col-lg-12">%data%</div>';
+//add titile and paragraph
+var bootstrapAddTitle = '<h3>%data%</h3>';
+var bootstrapAddParagraph = '<p>%data%</p>';
+var bootstrapAddImage = '<img src="%data%">';
+
+//top contact menu
+var startTopContactList = '<ul class="top-contact-element">%data%</ul>';
+var addTopContactElement = '<li>%data%</li>';
+
+var addHeaderImage = '<div class="col-md-12"><img src="%data%" alt="%alt%"></div>';
+var addWelcomeMessage = '<h3>%data%</h3>'
+
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
+
+
+var startSkills = '<div id="skills"><div class="container"><div class="row"><div class="col-sm-4"><div id="listOne"></div></div><div class="col-sm-4"><div id="listTwo"></div></div></div></div></div>';
+
+var startListSkills = '<ul id="ulSkills"><li class="list-entry">%title%<span class="star-group">%stars%</span></li></ul>';
+var addOneStar = '<img src="%data%" alt="img not found"></img>';
+
+
+var addNavElement = '<li><a href="#%data%">%name%</a></li>';
+
+
+var addProcessUL = '<ul>%data%</ul>';
+var addProcessElement = '<li>%data%</li>';
+
+
+
+
+
+
+
+
+//stuff from the old site
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+
+
+
+
+
+
---
+++ resume/js/resumeBuilder.js
@@ -0,0 +1,325 @@
+
+
+
+//top contact list elements
+var topContactList = {
+	'element1':'Email',
+	'element2':'Phone',
+	'element3':'Twitter',
+	'element4':'Linkedin',
+};
+
+function topContact(list1){
+	for(el in list1){
+		var formattedAddTopContactElement = addTopContactElement.replace('%data%', topContactList[el]);
+		var formattedStartTopContactList = startTopContactList.replace('%data%', formattedAddTopContactElement);
+		$('#top-contact').append(formattedStartTopContactList);
+	};
+};
+topContact(topContactList);
+
+
+
+
+
+
+
+
+
+
+//Header image 
+var imageList = {
+	'headerImage':'/Users/cswright88/Desktop/Resume-WEB-CSW/images/hello-world2.png',
+	'headerAlt':'Hello World',
+};
+function addHeaderImage1(){
+	var formattedaddHeaderImage = addHeaderImage.replace('%data%',imageList.headerImage).replace('%alt%',imageList.headerAlt);
+	$('#header-background-photo').append(formattedaddHeaderImage);
+};
+addHeaderImage1();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Bio list
+var bio = {
+	'fullName':'Christopher Wright',
+	'welcome':"I'm Chris",
+}
+function welcomeMessage(){
+	formattedaddWelcomeMessage = addWelcomeMessage.replace('%data%', bio.welcome);
+	$('#header').append(formattedaddWelcomeMessage);
+};	
+welcomeMessage();
+
+
+
+
+
+
+
+var navList = {
+	'workExperience':'Work Experience',
+	'projects':'Projects',
+	'about':'About',
+	'whyMe':'Why Me',
+	'process':'How I Work',
+	'contact':'Contact',
+};
+for(i in navList){
+	var formattedaddNavElement = addNavElement.replace('%data%', i).replace('%name%', navList[i]);
+	$('.nav').append(formattedaddNavElement);
+};
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+
+
+
+
+
+
+//work and projects
+var work = {
+	"jobs":[
+		{
+			"employer":"Fidelity Investments",
+			"title":"Defined Contributions Representative",
+			"location":"Westlake",
+			"date":"August 2011 - December 2012",
+			"description":"processed loans withdrawals and trades in the Fidelity 401k product"
+		},
+		{
+			"employer":"Luxe Valet",
+			"title":"Logistics Coordinator",
+			"location":"Austin",
+			"date":"August 2016 - current",
+			"description":"Managed between 20-30 valets in over 6 different cities nationwide"
+		},
+		{
+			"employer":"The Wandering Palette",
+			"title":"Website Developer",
+			"location":"Austin",
+			"date":"December 2015 - December 2016",
+			"description":"Designed and maintained the website for the Wandering Palette"
+		},
+	]
+};
+var projects = {
+	"Hertz":{
+		"title":"Hertz On-Demand App",
+		"dates":"December 2016 - Current",
+		"description":"QA tester for App and Birdseye software to launch the Hertz On-Demand App"
+	},
+	"ManualCustomerCharge":{
+		"title":"Accounts recievable policy",
+		"dates":"February 2017 - March 2017",
+		"description":"Created a time saving manual proceedure to charge customers in for spcialized services quickly to increase the productivity of the Logistics coordinator role",
+	}
+};
+function displayWork(){
+	for(job in work.jobs){
+		$('#workExperience').append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDate = HTMLworkDates.replace("%data%", work.jobs[job].date);
+		$('.work-entry:last').append(formattedDate);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$('.work-entry:last').append(formattedDescription);
+	};
+};
+displayWork();
+function displayProjects(){
+	for(project in projects){
+		$('#projects').append(HTMLprojectStart);
+		 formattedHTMLprojectTitle = HTMLprojectTitle.replace('%data%', projects[project].title);
+		 formattedHTMLprojectDates = HTMLprojectDates.replace('%data%', projects[project].dates);
+		 formattedHTMLprojectDescription = HTMLprojectDescription.replace('%data%', projects[project].description);
+		 allTogether2 = formattedHTMLprojectTitle+formattedHTMLprojectDates+formattedHTMLprojectDescription;
+		$('.project-entry:last').append(allTogether2);
+	};
+};
+displayProjects();
+
+
+
+
+
+
+//elements in the bootstrap columns (3) 4 columns header and paragraph
+var small3colList = {
+	'title1':'I Fix Things',
+	'paragraph1':'Ever since I was a child I enjoyed solving puzzles.  My perspective on the world was and still is through the lens of puzzles.  I made my first website in college and I knew right then that I was hooked.  I was obsessed with putting elements together in new and efficient ways.  I loved each new theory and tactic that came out every year.',
+	'title2':'Column 2',
+	'paragraph2':'I couldn’t stop learning and reading about new techniques and I wanted to know how it all worked from the code to the final product.  In the beginning I used my skills to start businesses for myself and friends and family. I didn’t seek education at college because this was the one thing that I could find everything I wanted to learn online.  I am now completing an online certification through Udacity to gain some credentials beyond my projects.',
+	'title3':'Column 3',
+	'paragraph3':'As I breeze through the course material I become more and more excited about finding myself in my future full time career as a front end web developer. As I grow and develop I learn more and more about myself, I’ve learned about my weaknesses and strengths and I know how to apply my skills or find a solution when I don’t know the answer.  AKA ask the Oracle (google).  Reach out to me if you have any questions, follow me on social media.  ',
+};
+function small3col(div, list){
+	var formattedbootstrapAddTitle1 = bootstrapAddTitle.replace('%data%', list.title1);
+	var formattedbootstrapAddParagraph1 = bootstrapAddParagraph.replace('%data%', list.paragraph1);
+	var formattedbootstrapAddTitle2 = bootstrapAddTitle.replace('%data%', list.title2);
+	var formattedbootstrapAddParagraph2 = bootstrapAddParagraph.replace('%data%', list.paragraph2);
+	var formattedbootstrapAddTitle3 = bootstrapAddTitle.replace('%data%', list.title3);
+	var formattedbootstrapAddParagraph3 = bootstrapAddParagraph.replace('%data%', list.paragraph3);
+
+	var formattedbootstrapSmall4Start1 = bootstrapSmall3Start.replace('%data%', formattedbootstrapAddTitle1+formattedbootstrapAddParagraph1);
+	var formattedbootstrapSmall4Start2 = bootstrapSmall3Start.replace('%data%', formattedbootstrapAddParagraph2);
+	var formattedbootstrapSmall4Start3 = bootstrapSmall3Start.replace('%data%', formattedbootstrapAddParagraph3);
+	var allTogether = formattedbootstrapSmall4Start1+formattedbootstrapSmall4Start2+formattedbootstrapSmall4Start3;
+
+	var formattedbootstrapRowStart = bootstrapRowStart.replace('%data%',allTogether);
+	var formattedbootstrapContainerStart = bootstrapContainerStart.replace('%data%',formattedbootstrapRowStart);
+
+	$(div).append(formattedbootstrapContainerStart);
+};
+small3col('#whyMe', small3colList);
+small3col('#contact', small3colList);
+
+
+
+
+
+
+var processList = [
+"-outline",
+"-Visualize",
+"-Solve problems",
+"-write Tests",
+"-caffein",
+"-Code",
+"-make confused faces",
+"-code more",
+"-Launch ",
+];
+for(i in processList){
+	var formattedaddProcessElement = addProcessElement.replace('%data%', processList[i]);
+	var formattedaddProcessUL = addProcessUL.replace('%data%',formattedaddProcessElement);
+	$('#process').append(formattedaddProcessUL);
+};
+
+
+
+
+
+//about me skills - there are two bc Im stupid and cant figure out one
+//just try to make them even
+
+var skillsL1 = {
+	'HTML5':5,
+	'CSS3':5,
+	'JavaScript':4,
+	'PHP':3,
+	'Photoshop':2,
+	'MySQL':1,
+	'Sleep':0,
+	'JSON':1,
+};
+var skillsL2 = {
+	'SEO':3,
+	'WordPress':3,
+	'AngularJS':2,
+	'OOP':4,
+	'JAVA':4,
+	'Bootstrap':4,
+	'JQuery':4,
+	'Command line':4,
+};
+function displaySkills(){
+	var starBlack = '/Users/cswright88/Desktop/Resume-WEB-CSW/images/starBlack.png';
+	var starWhite = '/Users/cswright88/Desktop/Resume-WEB-CSW/images/starWhite.png';
+	var formattedaddOneWhiteStar = addOneStar.replace('%data%', starWhite);
+	var formattedaddOneBlackStar = addOneStar.replace('%data%', starBlack);
+	function stars(num){
+		
+		if(num === 0){
+			return formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar;
+		};
+		if(num === 1){
+			return formattedaddOneBlackStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar;
+		};
+		if(num === 2){
+			return formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar;
+		};
+		if(num === 3){
+			return formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneWhiteStar+formattedaddOneWhiteStar;
+		};
+		if(num === 4){
+			return formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneWhiteStar;
+		};
+		if(num === 5){
+			return formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar+formattedaddOneBlackStar;
+		};
+	};
+	$('#about').append(startSkills);
+	$('#skills').prepend('<h3>Skills</h3>');
+	function list1(){
+		for(i in skillsL1){
+			var FstartListSkills1 = startListSkills.replace('%title%', i).replace('%stars%', (stars(skillsL1[i])));
+			$('#listOne').append(FstartListSkills1);
+		};
+	};
+	function list2(){
+		for(c in skillsL2){
+			var FstartListSkills2 = startListSkills.replace('%title%', c).replace('%stars%', (stars(skillsL2[c])));
+			$('#listTwo').append(FstartListSkills2);
+		};
+	};
+
+	list1();
+	list2();
+};
+displaySkills();
+
+$(".list-entry").hover(function(){
+    $(this).css("background-color", "lightgrey");
+    }, function(){
+    $(this).css("background-color", "initial");
+});
+$(".list-entry").hover(function(){
+	$(this,".star-group img").css("margin-right", "10px");
+}, function(){
+	$(this,".lstar-group img").css("margin-right", "initial");
+});
+
+
+
+
+
+
+
+
+
+
+
