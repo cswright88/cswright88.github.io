7e57ebaf56a542019a78303b2c58bcb343e49887
cswright88 <cswright88@gmail.com>
2017-04-11 15:15:01 -0500

    try to prevent FSL
    fsl

--- performance_Project/views/js/main.js
+++ performance_Project/views/js/main.js
@@ -500,10 +500,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-
+  var scrolltop = document.body.scrollTop;
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
