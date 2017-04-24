fcd974db34ec7a098f9c076a8b7fe271f587d44b
cswright88 <cswright88@gmail.com>
2017-04-24 13:34:36 -0500

    street view must be http not https even if there is a warning

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -16,7 +16,7 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = formStreet+' '+cityStr;
     $greeting.text('So, you want to live at '+ address+'?');
-    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
     $body.append('<img class="bgimg" src="'+streetviewUrl+'">');
 
     // YOUR CODE GOES HERE!
