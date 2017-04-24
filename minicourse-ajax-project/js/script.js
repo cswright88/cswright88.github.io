d37dd655bd47dd29c17da599e8e616024b1b980d
cswright88 <cswright88@gmail.com>
2017-04-24 13:30:32 -0500

    added https for the loading of apis for when using the GitHub active site

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -16,12 +16,12 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = formStreet+' '+cityStr;
     $greeting.text('So, you want to live at '+ address+'?');
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
     $body.append('<img class="bgimg" src="'+streetviewUrl+'">');
 
     // YOUR CODE GOES HERE!
     //NY times articles
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=1e80463dfad44f34a70595b132c486ba';
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=1e80463dfad44f34a70595b132c486ba';
     
     $.getJSON(nytimesUrl, function(data) {
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -43,7 +43,7 @@ function loadData() {
     });
 
     //wikipedia articles
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr + '&format=json&callback=wikiCallback';
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr + '&format=json&callback=wikiCallback';
     //to handle errors for .ajax requests you can use that .error chair or this timout request but clear the timout if the success function is run so that it doesnt run even if the articles do load
     
 
